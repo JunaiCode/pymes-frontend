a67563399e83b682de5672fbda31c1122075b0dc
Mateo Rada <mateora73@gmail.com>
2024-04-10 22:53:06 -0500

    Sidebar colapse works

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,16 +1,30 @@
-import AdminSidebar from "@/components/admin/AdminSidebar";
-import SidebarItem from "@/components/admin/SidebarItem";
+import AdminSidebar,{SidebarItem} from "@/components/admin/AdminSidebar";
 
-import { IoMdHome, IoMdPerson, IoMdSettings } from "react-icons/io";
+
+import {
+  IoHomeOutline, IoShapesOutline, IoBusiness,
+  IoPricetagOutline, IoColorFilterOutline,
+  IoPodiumOutline, IoHelpCircleOutline,
+  IoAnalytics 
+} from "react-icons/io5";
+
+
+
 
 function adminHome() {
   let userType = "admin";
-  if(userType === "admin") {
+  if (userType === "admin") {
     return (
       <div>
         <AdminSidebar>
-          <SidebarItem icon={<IoMdHome size={25} className="text-white"/>} text="Home" route="/admin" />
-
+          <SidebarItem icon={<IoHomeOutline size={25} className="text-white" />} text="Inicio" route="/home" />
+          <SidebarItem icon={<IoShapesOutline size={25} className="text-white" />} text="Modelo" route="/model" />
+          <SidebarItem icon={<IoBusiness size={25} className="text-white" />} text="Empresas" route="/companies" />
+          <SidebarItem icon={<IoPricetagOutline size={25} className="text-white" />} text="Tags" route="/tags" />
+          <SidebarItem icon={<IoColorFilterOutline size={25} className="text-white" />} text="Dimensiones" route="/colors" />
+          <SidebarItem icon={<IoPodiumOutline size={25} className="text-white" />} text="Niveles" route="/levels" />
+          <SidebarItem icon={<IoHelpCircleOutline size={25} className="text-white" />} text="Preguntas" route="/questions" />
+          <SidebarItem icon={<IoAnalytics size={25} className="text-white" />} text="Reportes" route="/reports" />
         </AdminSidebar>
 
       </div>
@@ -20,7 +34,7 @@ function adminHome() {
   return (
     <div>
       <h1>User home
-      
+
       </h1>
     </div>
   );
--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -1,49 +1,74 @@
 'use client'
 import { useRouter } from "next/navigation";
-import { IoMdArrowBack } from "react-icons/io";
+import { IoArrowBackOutline,IoArrowForward} from "react-icons/io5";
 import { IoIosLogOut } from "react-icons/io";
+import { createContext, useState } from "react";
+import React, {useContext} from 'react';
 
+const SidebarContext = createContext({ expanded: true, setExpanded: (expanded: boolean) => {} });
 
-
-const AdminSidebar = ({children}: {children: any}) => {
+export default function AdminSidebar({ children }: { children: any }){
     const router = useRouter();
+    const [expanded, setExpanded] = useState(true);
     return (
         <>
-            <aside className="h-screen flex flex-col bg-dark_bg max-w-40">
-                <div className="p-4 pb-2 flex justify-between items-center">
-                    <p className="font-sans font-bold text-white text-2xl">Logo</p>
-                    <button className=" hover:bg-secondary p-2 rounded-full transition-colors duration-100 ease-in-out" >
-                        <IoMdArrowBack className="text-white " />
+            <aside className={`h-screen flex flex-col bg-dark_bg transition-all duration-300 ease-in-out ${expanded ? "w-52" : "w-20"}`}>
+                <div className="p-4 pb-8 flex justify-between items-center">
+                    <p className={`font-sans font-bold text-white text-2xl mx-2
+                        overflow-hidden transition-all ${expanded ? "w-full" : "w-0"}
+                    `}>Logo</p>
+                    <button className="hover:bg-secondary p-2 rounded-full transition-colors duration-100 ease-in-out"
+                        onClick={() => setExpanded(!expanded)}
+                    >
+                        {expanded ?  <IoArrowForward className="text-white transform rotate-180" />: <IoArrowBackOutline className="text-white transform rotate-180" /> }
                     </button>
                 </div>
 
-                <ul className="flex-1 px-3">{children}</ul>
+                <SidebarContext.Provider value={{ expanded, setExpanded }}>
+                    <ul className="flex flex-col items-center ">
+                        {children}
+                    </ul>
+                </SidebarContext.Provider>
 
                 <div className="flex justify-center items-center p-4">
-                    <button 
-                        className="flex flex-row justify-between items-center
-                         hover:bg-dark_bg_hover p-2 rounded-lg transition-colors duration-100 ease-in-out px-6
-                         bg-primary"
+                    <button
+                        className={`flex flex-row ${expanded ? "justify-between bg-primary" : "justify-center"} items-center
+                         hover:bg-dark_bg_hover p-2 rounded-lg duration-100 px-3
+                          transition-all`}
                     >
-                        <IoIosLogOut size={25} className="text-white mr-4"/>
-                        <p className="text-white text-lg">Salir</p>
+                        <IoIosLogOut size={25} className={`text-white ${expanded ? "mr-2" : ""}` } />
+                        {expanded ? <p className="text-white text-lg ">Salir</p> : null}
+                        
                     </button>
                 </div>
             </aside>
 
-           
+
 
         </>
     )
 }
 
-export default AdminSidebar;
 
-export function SidebarItem({icon, text, route}) {
-    return (
-        <li >
-            {icon}
-            <p className="text-white ml-4">{text}</p>
-        </li>
-    )
+
+interface SidebarItemProps {
+    icon: JSX.Element;
+    text: string;
+    route: string;
 }
+
+export function SidebarItem({icon, text, route}: SidebarItemProps){
+    const {expanded} = useContext(SidebarContext);
+    return (
+        <li className="w-full px-3">
+            <button className="flex flex-row px justify-start items-start hover:bg-dark_bg_hover rounded-lg transition-colors duration-300 ease-in-out
+            h-fit w-full px-3 py-1.5 my-4">
+                {icon}
+                {expanded ? <p className={`text-white text-lg mx-3 overflow-hidden transition-all `}>{text}</p> : null}
+                
+            </button>
+
+        </li>
+    );
+}
+
