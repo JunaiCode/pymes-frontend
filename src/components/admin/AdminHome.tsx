943753855814e3f8393c4aee9db889373c0e7fb2
Mateo Rada <mateora73@gmail.com>
2024-04-13 19:12:28 -0500

    Model page

--- src/components/admin/AdminHome.tsx
+++ src/components/admin/AdminHome.tsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, Rectangle, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 const data = [
     {
@@ -47,6 +47,22 @@ const data = [
 ];
 
 export default function AdminHome() {
+    let totalEvaluations = 520;
+    let userStats = [
+        {
+            name: 'Usuarios registrados',
+            value: 120
+        },
+        {
+            name: 'Usuarios recurrentes',
+            value: 20
+        },
+        {
+            name: 'Re-evaluaciones',
+            value: 10
+        },
+
+    ];
     return (
         <div className="w-full flex flex-col items-start justify-center bg-light">
             <div className="w-full p-10">
@@ -55,32 +71,61 @@ export default function AdminHome() {
             <div className="w-full h-full flex flex-row  p-4">
                 <div className="h-full w-full flex flex-col bg-white mr-2 border rounded-lg shadow-lg p-4" >
                     <p className="text-2xl font-sans font-bold">Evaluaciones recientes</p>
-                    <ResponsiveContainer 
-                        width="100%" 
+                    <ResponsiveContainer
+                        width="100%"
                         height="100%">
-                        <BarChart 
-                            width={150} 
-                            height={40} 
+                        <BarChart
+                            width={150}
+                            height={40}
                             data={data}
                             margin={{
                                 top: 50,
                                 bottom: 5,
-                              }}
-                            >
+                            }}
+                        >
                             <Legend />
-                            <Bar dataKey="uv" fill="#202f75" />
+                            <Bar dataKey="uv" fill="#202f75" activeBar={<Rectangle fill="#202f75" radius={[5, 5, 0, 0]} />} />
                             <XAxis dataKey="name" />
                             <YAxis />
                             <Tooltip />
-                            
+
                         </BarChart>
                     </ResponsiveContainer>
                 </div>
-                <div className="h-full w-full bg-white ml-2 border rounded-lg shadow-lg" >
-
+                <div className="h-full w-full flex flex-col" >
+                    <div className="h-fit w-full bg-white ml-2 border rounded-lg shadow-lg mb-2 p-4">
+                        <p className="text-2xl font-sans font-bold">Total de evaluaciones</p>
+                        <p className="text-2xl font-sans">{totalEvaluations}</p>
+                    </div>
+                    <div className="h-full w-full bg-white ml-2 border rounded-lg shadow-lg p-4">
+                        <p className="text-2xl font-sans font-bold">Estadisticas de los usuarios</p>
+                        <div className="flex flex-col w-full">
+                            {userStats.map((stat, index) => (
+                                <StatCard key={index} title={stat.name} value={stat.value.toString()} />
+                            ))}
+                        </div>
+                    </div>
                 </div>
             </div>
 
+        </div>
+    );
+}
+
+interface StatCardProps {
+    title: string;
+    value: string;
+}
+
+export function StatCard({ title, value }: StatCardProps) {
+    return (
+        <div className="h-fit w-full flex bg-white ml-2 border rounded-lg shadow-lg my-2">
+            <div className="flex w-3/4 h-full bg-slate-300 rounded-tl-lg rounded-bl-lg p-2">
+                <p className="text-2xl font-sans font-bold  ">{title}</p>
+            </div>
+            <div className="flex items-center  justify-center w-1/4">
+                <p className="text-2xl font-sans">{value}</p>
+            </div>
         </div>
     );
 }
