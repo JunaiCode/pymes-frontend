50972806f5e13f574c6c7548c1ee6c510bc7df37
JunaiCode <juanpalta24@hotmail.com>
2024-05-03 19:51:39 -0500

    bar progress tailwind

--- src/components/company/ProgressRoadMap.tsx
+++ src/components/company/ProgressRoadMap.tsx
@@ -1,7 +1,9 @@
 export const ProgressRoadMap = () => {
     return (
-        <div className="w-1/2 flex flex-col justify-center items-center">
-        <progress className="w-full rounded-xl" value="20" max="100"/>
-        </div>
+    <div className="w-1/2 flex flex-col justify-center items-center">
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+    <div className="bg-primary h-2.5 rounded-full w-1/2"></div>
+    </div>
+    </div>
     )
 };
