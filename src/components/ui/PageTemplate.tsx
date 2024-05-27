9aa3d8df6f967fd3722dd13cdbb6a4c0c52bf619
JunaiCode <juanpalta24@hotmail.com>
2024-05-27 10:29:48 -0500

    edit pageTemplate

--- src/components/ui/PageTemplate.tsx
+++ src/components/ui/PageTemplate.tsx
@@ -3,14 +3,12 @@ import AdminSidebar from '../admin/AdminSidebar';
 import CompanySideBar from '../company/CompanySideBar';
 
 export default function PageTemplate({ children }: { children: any }) {
-  let userType = 'company';
+  let userType = 'admin';
   if (userType === 'admin') {
     return (
       <div className="flex flex-row">
         <AdminSidebar />
-        <div className="flex flex-col w-full">
           {children}
-        </div>
       </div>
     );
   } else {
