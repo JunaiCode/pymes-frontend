baa196b378ff73079a996fd596b3d311b8b622c4
JunaiCode <juanpalta24@hotmail.com>
2024-05-28 13:06:16 -0500

    Edit CompanySideBar

--- src/components/company/CompanySideBar.tsx
+++ src/components/company/CompanySideBar.tsx
@@ -28,7 +28,7 @@ export default function CompanySideBar() {
   return (
     <>
       <aside
-        className={`h-100 flex flex-col bg-dark_bg transition-all duration-300 ease-in-out ${expanded ? "w-64" : "w-20"
+        className={`h-screen sticky left-0 top-0 flex flex-col m-0 bg-dark_bg transition-all duration-300 ease-in-out ${expanded ? "w-64" : "w-20"
           }`}
       >
         <div className="p-4 pb-8 flex justify-between items-center">
